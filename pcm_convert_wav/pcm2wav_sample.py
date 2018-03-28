e5a43a1c5000061796b8493b8d0c5a19f3ded897
Lung yu-Tsai <workfile975@gmail.com>
2018-03-28 15:46:12 +0800

    update lib path

--- pcm_convert_wav/pcm2wav_sample.py
+++ pcm_convert_wav/pcm2wav_sample.py
@@ -1,7 +1,7 @@
 class tygrlib:
     def __init__(self):
         from ctypes import cdll
-        self.lib = cdll.LoadLibrary('libpcm2wav.so')
+        self.lib = cdll.LoadLibrary('./libpcm2wav.so')
 
     def pcm2wav(self,src_filename,dst_filename):
         res = self.lib.pcm2wav(src_filename,dst_filename)
